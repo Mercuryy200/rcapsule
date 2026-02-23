4eb986ba1e2251d28df85dbdc1ff79d67a4315a1
Rima Nafougui <Rimanafougui99@gmail.com>
2026-02-23 06:11:18 -0500

    Update fonts.ts

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,9 +1,11 @@
-import { Fira_Code as FontMono, DM_Sans as FontSans } from "next/font/google";
+import {
+  Josefin_Sans as FontMono,
+  Montserrat as FontSans,
+} from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-  weight: "400",
 });
 
 export const fontMono = FontMono({
